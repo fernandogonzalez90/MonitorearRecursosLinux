2b6cc1bab00cdfcf3bff64cea93cfcf1862f14cc
fernandogonzalez90 <fer.ema.gonzalez@gmail.com>
2023-12-05 11:52:53 -0300

    se añade % y total, se redondea a 1 deciamal

--- api.py
+++ api.py
@@ -7,28 +7,42 @@ app = FastAPI()
 
 class SystemInfo(BaseModel):
     processor_info: str
-    ram_info: int
-    disk_info: int
+    ram_total: float
+    ram_used_percent: float
+    disk_total: float
+    disk_used_percent: float
 
 @app.get("/get_system_info")
 def get_system_info():
     # Obtener información del sistema
     processor_info = platform.processor()
-    ram_info = psutil.virtual_memory().total
-    disk_info = psutil.disk_usage('/').total
+    
+    # Información de RAM
+    ram_info = psutil.virtual_memory()
+    ram_total_gb = round(ram_info.total / (1024 ** 3), 1)
+    ram_used_percent = round(ram_info.percent, 1)
+
+    # Información del disco
+    disk_info = psutil.disk_usage('/')
+    disk_total_gb = round(disk_info.total / (1024 ** 3), 1)
+    disk_used_percent = round(disk_info.percent, 1)
 
     # Construir la respuesta
     system_info = SystemInfo(
         processor_info=processor_info,
-        ram_info=ram_info,
-        disk_info=disk_info
+        ram_total=ram_total_gb,
+        ram_used_percent=ram_used_percent,
+        disk_total=disk_total_gb,
+        disk_used_percent=disk_used_percent
     )
 
     # Formatear la respuesta
     formatted_response = {
         "Processor Info": system_info.processor_info,
-        "RAM Info (bytes)": system_info.ram_info,
-        "Disk Info (bytes)": system_info.disk_info
+        "RAM Total (GB)": system_info.ram_total,
+        "RAM Used Percentage": system_info.ram_used_percent,
+        "Disk Total (GB)": system_info.disk_total,
+        "Disk Used Percentage": system_info.disk_used_percent
     }
 
     return formatted_response
